25a8760951194fda2dbde0bb2ec52de55a24aeec
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-26 13:30:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function onesDigit(n) {
   // your code here
-	
-	return n[n.length-1]
-}
 
+	// return n[n.length-1]
+	let digit = n % 10;
+	return digit;
+}
 // Do not change the code below
 
 const n = prompt("Enter Number:");
